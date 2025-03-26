c61a78c9f700b5acd86e0e78d1cb8c5ebc58e1b5
Logan Nelsen <loganjnelsen@gmail.com>
2025-03-26 13:04:21 -0500

    ignore eslint during build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   // images: {
   //   domains: ["wakes-media.s3.amazonaws.com"],
   // },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
